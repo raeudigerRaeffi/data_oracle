18b270c1c8bc88b106651b63ddb5ecff23bd29f0
raeudigerraffi <raffaelschon@rocketmail.com>
2023-11-07 07:59:51 +0100

    first commit

---
+++ src/data_oracle/connectors/baseconnector.py
@@ -0,0 +1,67 @@
+
+
+class BaseDBConnector:
+
+    def __init__(self,connection_data):
+        self.connection = self.connect(connection_data)
+        pass
+
+    def connect(self,connection_data):
+        """
+        Returns a connection object to DB
+        """
+        pass
+
+    def is_available(self):
+        """
+        Returns Boolean whether connections is successful
+        """
+        pass
+
+    def return_table_names(self):
+        """
+        Returns list of table names
+        """
+        pass
+
+    def return_view_names(self):
+        """
+        Returns list of view names
+        """
+        pass
+
+    def return_table_columns(self,table_name):
+        """
+        @table_name:str
+        Returns list of columns of table
+        """
+        pass
+
+    def execute_sql_statement(self,_sql):
+        """
+        @_sql:str
+        Returns result of sql statement
+        """
+
+        pass
+
+    def return_table_column_info(self):
+        """
+        Returns dictionary containing table_name : [column_name] pairs
+        """
+        all_tables = self.return_table_names()
+        return { x:self.return_table_columns(x) for x in all_tables}
+
+    def return_view_column_info(self):
+        """
+        Returns dictionary containing table_name : [column_name] pairs
+        """
+        all_tables = self.return_view_names()
+        return { x:self.return_table_columns(x) for x in all_tables}
+
+    def return_db_layout(self):
+        return {
+            "views": self.return_view_column_info(),
+            "tables": self.return_table_column_info()
+        }
+
---
+++ src/data_oracle/connectors/sqlalchemyconnector.py
@@ -0,0 +1,81 @@
+import asyncio
+from .baseconnector import BaseDBConnector
+
+from sqlalchemy import create_engine, URL, inspect, text
+from overrides import override
+
+
+class SqlAlchemyConnector(BaseDBConnector):
+    """
+    Connector available for all
+    """
+    def __init__(self,connection_data):
+        super().__init__(connection_data)
+
+    @override
+    def connect(self,connection_data):
+        #connection_data.database_type
+        drivername_mapper = {
+            'MySQL' : 'mysql+pymysql',
+            'PostgreSQL' : 'postgresql+psycopg2',
+            'Oracle' : 'oracle+oracledb',
+            'Microsoft SQL Server' : 'mssql+pyodbc',
+            'SQLite' : 'sqlite',
+        }
+        driver = drivername_mapper[connection_data.database_type]
+        url_object = URL.create(
+            driver,
+            username = connection_data.username,
+            password = connection_data.password,
+            host = connection_data.host,
+            port = connection_data.port,
+            database= connection_data.database_name,
+        )
+        return create_engine(url_object)
+
+    @override
+    def is_available(self):
+        """
+        Returns Boolean whether connections is successful
+        """
+        try:
+            self.connection.connect()
+            return True
+        except:
+            return False
+
+    @override
+    def return_table_names(self):
+        """
+        Returns list of table names
+        """
+        return inspect(self.connection).get_table_names()
+
+    @override
+    def return_view_names(self):
+        """
+        Returns list of view names
+        """
+        return inspect(self.connection).get_view_names()
+
+    def return_all_table_column_info(self,table_name):
+        return inspect(self.connection).get_columns(table_name)
+
+
+    @override
+    def return_table_columns(self,table_name):
+        """
+        Returns list of columns of table
+        """
+        all_info = self.return_all_table_column_info(table_name)
+        return [x['name'] for x in all_info]
+
+    @override
+    def execute_sql_statement(self,_sql):
+        """
+        @_sql:str
+        Returns result of sql statement
+        """
+        with self.connection.connect() as conn:
+            result = conn.execute(text(_sql))
+        return result
